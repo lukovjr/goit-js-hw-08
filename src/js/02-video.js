347060ae4ba1be803291f9db94fdb5cee14d8152
Oleksy Lykov <lukov_jr@ukr.net>
2023-03-14 14:54:44 +0200

    2/3 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import throttle from "lodash.throttle";   
+import Player from "@vimeo/player";
 
+const player = new Player('vimeo-player');
+
+const CURRENT_TIME = "videoplayer-current-time";
+
+player.on("timeupdate", throttle(getCurrentTime, 1000));
+
+function getCurrentTime(data) {
+    localStorage.setItem(CURRENT_TIME, data.seconds);
+}
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME) || 0);
